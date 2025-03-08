45227ea45efd802d83f397feffa87aa34031c4e2
Jasper Janin <jasper.janin@me.com>
2025-03-08 16:36:42 +0100

    refactor, general build executable

--- fm-index-builder/src/main.rs
+++ fm-index-builder/src/main.rs
@@ -1,43 +1,54 @@
-use std::error::Error;
-use fm_index::{BackwardSearchIndex, FMIndex};
-use fm_index::suffix_array::{NullSampler, SuffixOrderSampler};
 use fm_index::converter::RangeConverter;
+use fm_index::suffix_array::SuffixOrderSampler;
+use fm_index::{BackwardSearchIndex, FMIndex};
+use fm_index_benchmarking::benchmarker::try_from_database_file_uncompressed_with_length;
+use fm_index_benchmarking::{load_index_postcard, save_index_postcard};
+use std::io::Result;
+use clap::Parser;
 
-use std::fs;
-
-fn main() -> Result<(), Box<dyn Error>> {
-    let text = fs::read_to_string("unipept-index-data/proteins.tsv")
-        .expect("Should have been able to read the file")
-        .as_bytes()
-        .to_vec();
-
-    // Converter converts each character into packed representation.
-    // `' '` ~ `'~'` represents a range of ASCII printable characters.
-    let converter = RangeConverter::new(b'\t', b'~');
-
-    // To perform locate queries, we need to retain suffix array generated in the construction phase.
-    // However, we don't need the whole array since we can interpolate missing elements in a suffix array from others.
-    // A sampler will _sieve_ a suffix array for this purpose.
-    // You can also use `NullSampler` if you don't perform location queries (disabled in type-level).
-    // let sampler = SuffixOrderSampler::new().level(2);
-    let sampler = SuffixOrderSampler::new().level(32);
-    println!("Building index...");
+fn standard_build(infile: &str, outfile: &str, size_limit: usize, tsv_field: usize) {
+    println!("Starting builder...");
+    println!("Loading database...");
+    let text = try_from_database_file_uncompressed_with_length(infile, size_limit, tsv_field)
+        .unwrap()
+        .into_iter()
+        .map(|x| match x {
+            b'-' => b'@',
+            b'$' => b'@',
+            _ => x.clone(),
+        })
+        .collect::<Vec<u8>>();
+    println!("Done loading database");
+    let converter = RangeConverter::new(b'@', b'Z');
+    let sampler = SuffixOrderSampler::new().level(4);
+    println!("Spawned converter & sampler");
+    println!("Starting building index...");
     let index = FMIndex::new(text, converter, sampler);
-    println!("Done building index");
+    println!("Index built!");
+    println!("Saving to postcard binary...");
+    save_index_postcard(&index, outfile);
+    println!("Saved!");
+    println!("All done!")
+}
 
-    // Search for a pattern string.
-    let pattern = "VFPGLQGGPHNHTIGGLAVCLKHAQSPEFKAYQKRVVSNC";
-    println!("Searching...");
-    let search = index.search_backward(pattern);
-    println!("Done searching!");
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    input_tsv: String,
 
-    // Count the number of occurrences.
-    let n = search.count();
+    #[arg(short, long)]
+    output_bin: String,
 
-    println!("Matches: {n}");
+    #[arg(short, long, default_value_t = 0)]
+    size_limit: usize,
 
-    // List the position of all occurrences.
-    // let positions = search.locate();
+    #[arg(short, long, default_value_t = 2)]
+    tsv_field_index: usize,
+}
 
-    Ok(())
+fn main() -> Result<()> {
+    let args = Args::parse();
+    
+    Ok(standard_build(args.input_tsv.as_str(), args.output_bin.as_str(), args.size_limit, args.tsv_field_index))
 }
